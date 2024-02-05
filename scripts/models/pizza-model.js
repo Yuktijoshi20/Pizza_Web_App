7d1dc3ee7183f7c5add5eb20007e177595deae64
Yukti Joshi <106856890+Yuktijoshi20@users.noreply.github.com>
2024-02-05 12:26:55 +0530

    Add files via upload

---
+++ scripts/models/pizza-model.js
@@ -0,0 +1,15 @@
+//Data Modelling
+class Pizza{
+    constructor(id,name,price,url,desc){
+        //this - keyword
+        //hold the current calling object reference
+     this.id=id;
+     this.name=name;
+     this.price=price;
+     this.url=url;
+     this.desc=desc;
+     this.isAddedInCart=this.isAddedInCart;
+     
+    }
+}
+export default Pizza;
